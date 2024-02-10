4febc6abc0a9f37d13f0d7aa08e18e465abb3fd4
Chipada H <122068250+Hanzo-13@users.noreply.github.com>
2024-02-10 22:43:40 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Chipada</title>
+        <link rel="stylesheet" href="assets/css/style2.css">
+</head>
+<body>
+    
