d10570d0cdb0cbf8f0674f279eee63cf964cd0fb
Chipada H <122068250+Hanzo-13@users.noreply.github.com>
2024-02-10 22:59:51 +0800

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,36 @@
+<nav>
+    <div class="navbar">
+      <i class='bx bx-menu'></i>
+      <div class="logo"><a href="#">Websys-2</a></div>
+      <div class="nav-links">
+        <div class="sidebar-logo">
+          <span class="logo-name">Websys-2</span>
+          <i class='bx bx-x' ></i>
+        </div>
+        <ul class="links">
+          <li><a href="homepage.php"><b>HOME</b></a></li>
+          <li>
+            <a href="#"><b>ACTIVITIES</b></a>
+            <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
+            <ul class="htmlCss-sub-menu sub-menu">
+              <li><a href="Profile.php">Activity 1</a></li>
+              <li><a href="#">Activity 2</a></li>
+              <li><a href="#">Activity 3</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#"><b>ABOUT US</b></a>
+            <i class='bx bxs-chevron-down js-arrow arrow '></i>
+            <ul class="js-sub-menu sub-menu">
+              <li><a href="#">About Us</a></li>
+              <li><a href="Profile.php">Web Master</a></li>
+            </ul>
+          </li>
+
+          <li><alogin href="#">Login</alogin></li>
+
+        </ul>
+      </div>
+      </div>
+    </div>
+</nav>
